22ecca61bfb8b90d08a2fb4d22d4f4eceebf3641
akari <abow125@wgu.edu>
2026-02-02 19:26:18 -0800

    Updating risk score calculation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, status
 from schemas import TransactionRequest, RiskAssessment, TransactionStatus
+from logic.rules import check_large_transaction
 import uuid
 
 app = FastAPI(title="Real-Time Risk Monitor")
@@ -13,18 +14,27 @@ app = FastAPI(title="Real-Time Risk Monitor")
 async def assess_transaction_risk(transaction: TransactionRequest):
     
     # Receives a transaction, validates it via Pydantic, and returns a risk decision.
-    
-    # 1. Placeholder for Risk Logic
-    # Currently allows all.
-    decision = TransactionStatus.ALLOW
-    risk_score = 0
+    total_score = 0
     triggered_rules = []
+
+    large_transaction_score = check_large_transaction(transaction)
+    if large_transaction_score > 0:
+        total_score += large_transaction_score
+        triggered_rules.append("LARGE_TRANSACTION_DETECTION")
+
+    # Determines transaction's final risk score
+    if total_score > 71:
+        final_decision = TransactionStatus.DENY
+    elif total_score >= 31:
+        final_decision=TransactionStatus.REVIEW
+    else: 
+        final_decision=TransactionStatus.ALLOW
 
     # 2. Returns the assessment based on the response schema
     return RiskAssessment(
         transaction_id=transaction.transaction_id,
-        decision=decision,
-        risk_score=risk_score,
+        decision=final_decision,
+        risk_score=total_score,
         triggered_rules=triggered_rules
     )
 
